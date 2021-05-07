dd71cd833b2c665c9c86a9e42ee577b6c3c22369
Luisgustavo11 <luisgustavordp@gmail.com>
2021-05-06 22:56:03 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/luis-gustavo-ribeiro-de-paula-45584320a/" target="_blank" rel="noreferrer">Luis Gustavo R. de Paula</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
